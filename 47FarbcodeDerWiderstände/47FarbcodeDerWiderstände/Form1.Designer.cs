4c551c057b8758fac7eb010e7f130967cf3490e8
Lingg <01LinEli>
2018-03-02 12:33:42 +0100

    Programm angefangen; Anfang der Rechnungen

---
+++ 47FarbcodeDerWiderstände/47FarbcodeDerWiderstände/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace _47FarbcodeDerWiderstände
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.Berechne = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(340, 104);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(132, 22);
+            this.textBox5.TabIndex = 4;
+            // 
+            // Berechne
+            // 
+            this.Berechne.Location = new System.Drawing.Point(478, 93);
+            this.Berechne.Name = "Berechne";
+            this.Berechne.Size = new System.Drawing.Size(76, 45);
+            this.Berechne.TabIndex = 5;
+            this.Berechne.Text = "Berechne";
+            this.Berechne.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "schwarz",
+            "braun",
+            "rot",
+            "orange",
+            "gelb",
+            "grün",
+            "blau",
+            "violett",
+            "grau",
+            "weiß"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 49);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "schwarz",
+            "braun",
+            "rot",
+            "orange",
+            "gelb",
+            "grün",
+            "blau",
+            "violett",
+            "grau",
+            "weiß"});
+            this.comboBox2.Location = new System.Drawing.Point(154, 49);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 7;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "schwarz",
+            "braun",
+            "rot",
+            "orange",
+            "gelb",
+            "grün",
+            "blau",
+            "violett",
+            "grau",
+            "weiß"});
+            this.comboBox3.Location = new System.Drawing.Point(293, 49);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 24);
+            this.comboBox3.TabIndex = 8;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "grau   +-0.05%",
+            "violett   +-0.1%",
+            "blau   +-0.25%",
+            "grün   +-0.5%",
+            "braun   +-1%",
+            "rot   +-2%",
+            "gold   +-5%",
+            "silber   +-10%",
+            "keine   +-20%"});
+            this.comboBox4.Location = new System.Drawing.Point(433, 49);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 24);
+            this.comboBox4.TabIndex = 9;
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 16);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "1.Farbring";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(178, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 16);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "2.Farbring";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(317, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 16);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "3.Farbring";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(430, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 16);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "4.Farbring(Toleranz)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 192);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Berechne);
+            this.Controls.Add(this.textBox5);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Widerstands Rechner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button Berechne;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ 47FarbcodeDerWiderstände/47FarbcodeDerWiderstände/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _47FarbcodeDerWiderstände
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Farben(comboBox1);
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Farben(comboBox2);
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Farben(comboBox3);
+        }
+
+        private void Farben(ComboBox cb)
+        {
+            switch (cb.Text)
+            {
+                case "schwarz":
+                    {
+                        cb.BackColor = Color.Black;
+                        cb.ForeColor = Color.White;
+                    }
+                    break;
+                case "braun":
+                    {
+                        cb.BackColor = Color.Brown;
+                    }
+                    break;
+                case "rot":
+                    {
+                        cb.BackColor = Color.Red;
+                    }
+                    break;
+                case "orange":
+                    {
+                        cb.BackColor = Color.Orange;
+                    }
+                    break;
+                case "gelb":
+                    {
+                        cb.ForeColor = Color.Black;
+                        cb.BackColor = Color.Yellow;
+                    }
+                    break;
+                case "grün":
+                    {
+                        cb.BackColor = Color.Green;
+                    }
+                    break;
+                case "blau":
+                    {
+                        cb.ForeColor = Color.White;
+                        cb.BackColor = Color.Blue;
+                    }
+                    break;
+                case "violett":
+                    {
+                        cb.BackColor = Color.Violet;
+                    }
+                    break;
+                case "grau":
+                    {
+                        cb.BackColor = Color.Gray;
+                    }
+                    break;
+                case "weiß":
+                    {
+                        cb.ForeColor = Color.Black;
+                        cb.BackColor = Color.White;
+                    }
+                    break;
+
+            }
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox4.Text)
+            {
+                case "grau   +-0.05%":
+                    {
+                        comboBox4.BackColor = Color.Gray;
+                    }
+                    break;
+                case "violett   +-0.1%":
+                    {
+                        comboBox4.BackColor = Color.Violet;
+                    }
+                    break;
+
+                case "grün   +-0.5%":
+                    {
+                        comboBox4.BackColor = Color.Green;
+                    }
+                    break;
+                case "braun   +-1%":
+                    {
+                        comboBox4.BackColor = Color.Brown;
+                    }
+                    break;
+                case "rot   +-2%":
+                    {
+                        comboBox4.BackColor = Color.Red;
+                    }
+                    break;
+                case "gold   +-5%":
+                    {
+                        comboBox4.BackColor = Color.Gold;
+                    }
+                    break;
+                case "silber   +-10%":
+                    {
+                        comboBox4.BackColor = Color.Silver;
+                    }
+                    break;
+                case "keine   +-20%":
+                    {
+                        comboBox4.ForeColor = Color.Black;
+                        comboBox4.BackColor = Color.White;
+                    }
+                    break;
+                case "blau   +-0.25%":
+                    {
+                        comboBox4.ForeColor = Color.White;
+                        comboBox4.BackColor = Color.Blue;
+                    }
+                    break;
+            }
+        }
+       
+
+       
+
+
+    }
+}
